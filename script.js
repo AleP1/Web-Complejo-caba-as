bde8375770fd7a6e791a3fa53cf9fbc6ff45509a
AleP1 <63692378+AleP1@users.noreply.github.com>
2021-08-28 14:21:35 -0300

    clase3 - incorporar objetos y arrays

--- script.js
+++ script.js
@@ -1,45 +1,94 @@
-let reservas = false
+class Booking {
 
-while (reservas == false) {
+    constructor(checkInMonth, checkInDay, checkOutMonth, checkOutDay, name, email, phone) {
 
+        this.checkInMonth = checkInMonth;
+        this.checkInDay = checkInDay;
+        this.checkOutMonth = checkOutMonth;
+        this.checkOutDay = checkOutDay;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
 
-    const disponibilidad = () => {
-        let tipoCabaña = ''
-        let cabañas = parseInt(prompt("Indique la cantidad de huespedes para reservar una cabaña: \n 1 - 1 0 2 personas ---> cabaña pequeña \n 2 - 3 0 4 personas ---> cabaña mediana \n 3 - 5 0 6 personas ---> cabaña grande \n 4 - 7 o mas personas ---> cabaña muy grande"));
+    bookingData() {
+        let reservas = false
 
-        switch (cabañas) {
-            case 1:
-                tipoCabaña = 'cabaña pequeña'
-                break;
-            case 2:
-                tipoCabaña = 'cabaña mediana'
-                break;
-            case 2:
-                tipoCabaña = 'cabaña grande'
-                break;
-            case 4:
-                tipoCabaña = 'cabaña muy grande'
-                break;
-            default:
-                break;
+        while (reservas == false) {
+            
+            const disponibilidad = () => {
+                let tipoCabaña = ''
+                let cabañaElegida = false
+
+                let cabañas = parseInt(prompt("Indique la cantidad de huespedes para reservar una cabaña: \n 1 - 1 0 2 personas ---> cabaña pequeña \n 2 - 3 0 4 personas ---> cabaña mediana \n 3 - 5 0 6 personas ---> cabaña grande \n 4 - 7 o mas personas ---> cabaña muy grande"));
+
+                switch (cabañas) {
+                    case 1:
+                        tipoCabaña = 'cabaña pequeña'
+                        cabañaElegida = true
+                        break;
+                    case 2:
+                        tipoCabaña = 'cabaña mediana'
+                        cabañaElegida = true
+                        break;
+                    case 2:
+                        tipoCabaña = 'cabaña grande'
+                        cabañaElegida = true
+                        break;
+                    case 4:
+                        tipoCabaña = 'cabaña muy grande'
+                        cabañaElegida = true
+                        break;
+                    default:
+                        alert('La opción es incorrecta!')
+                        reservas = false
+                        cabañaElegida = false
+                        break;
+                }
+
+                if (((mesIngreso !== 'enero') && (mesIngreso !== 'febrero')) && (mesEgreso !== 'enero' && (mesEgreso !== 'febrero'))) {
+                    if(cabañaElegida === true){
+                        alert(`Su estadia será entre el ${diaIngreso} de ${mesIngreso} y el ${diaEgreso} de ${mesEgreso} en una ${tipoCabaña}. Será contactado a la brevedad.`)
+                        reservas = true
+                    }
+                } else {
+                    alert('Disculpe! No hay disponibilidad para la fecha seleccionada.')
+                    reservas = false
+                }
+            };
+            disponibilidad();
         }
-
-        let mesIngreso = prompt('Escriba el mes de ingreso')
-        let diaIngreso = parseInt(prompt(`Escriba el dia de igreso en ${mesIngreso}`))
-        let mesEgreso = prompt('Escriba el mes de egreso')
-        let diaEgreso = parseInt(prompt(`Escriba el dia de egreso en ${mesEgreso}`))
-
-
-        if (((mesIngreso !== 'enero') && (mesIngreso !== 'febrero')) && (mesEgreso !== 'enero' && (mesEgreso !== 'febrero'))) {
-
-            alert(`Su estadia será entre el ${diaIngreso} de ${mesIngreso} y el ${diaEgreso} de ${mesEgreso} en una ${tipoCabaña}. Será contactado a la brevedad.`)
-            reservas = true
-
-        } else {
-            alert('Disculpe! No hay disponibilidad para la fecha seleccionada.')
+        const data = {
+            checkInMonth: mesIngreso,
+            checkInDay: diaIngreso,
+            checkOutMonth: mesEgreso,
+            checkOutDay: diaEgreso,
+            name: nombre,
+            email: mail,
+            phone: telefono
         }
-    };
-
-    disponibilidad();
-
+        console.log(`${data}`);
+    }
 }
+
+const mesIngreso = prompt('Escriba el mes de ingreso')
+const diaIngreso = parseInt(prompt(`Escriba el dia de igreso en ${mesIngreso}`))
+const mesEgreso = prompt('Escriba el mes de egreso')
+const diaEgreso = parseInt(prompt(`Escriba el dia de egreso en ${mesEgreso}`))
+const nombre = prompt('Escriba su nombre')
+const mail = prompt('Escriba un Email')
+const telefono = prompt('Escriba su Telefono')
+
+const newBooking = new Booking(mesIngreso, diaIngreso, mesEgreso, diaEgreso, nombre, mail, telefono);
+newBooking.bookingData();
+
+//Array con todas las reservas
+
+const bookingList = [];
+bookingList.push(newBooking);
+console.log(`${bookingList}`);
+
+
+
+
+
